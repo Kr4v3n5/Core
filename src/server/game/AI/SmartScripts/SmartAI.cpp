6d9a7e006f4d5c356eb2d013b1935ddf5803e5e9
Venugh <Venugh@gmx.net>
2011-10-03 13:56:55 +0200

    merge to head tc.

--- src/server/game/AI/SmartScripts/SmartAI.cpp
+++ src/server/game/AI/SmartScripts/SmartAI.cpp
@@ -466,12 +466,18 @@ void SmartAI::EnterEvadeMode()
 
 void SmartAI::MoveInLineOfSight(Unit* who)
 {
-    if (!who) return;
+    if (!who)
+        return;
+    
     GetScript()->OnMoveInLineOfSight(who);
+    
     if (me->HasReactState(REACT_PASSIVE) || AssistPlayerInCombat(who))
         return;
 
     if (!CanAIAttack(who))
+        return;
+    
+    if (!me->canStartAttack(who, false))
         return;
 
     if (me->IsHostileTo(who))
@@ -491,9 +497,6 @@ void SmartAI::MoveInLineOfSight(Unit* who)
             }
         }
     }
-
-    //if (me->canStartAttack(who, false))
-    //    AttackStart(who);
 }
 
 bool SmartAI::CanAIAttack(const Unit* /*who*/) const
