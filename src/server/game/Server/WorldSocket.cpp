1af7da5adcb33f2bf5547e10e902155f58195769
Venugh <venugh@gmx.net>
2012-04-15 22:00:13 +0200

    some typos.

--- src/server/game/Server/WorldSocket.cpp
+++ src/server/game/Server/WorldSocket.cpp
@@ -916,7 +916,7 @@ int WorldSocket::HandleAuthSession(WorldPacket& recvPacket)
     else
     {
         fields = result->Fetch();
-        security = fields[1].GetInt32();
+        security = fields[0].GetUInt8();
     }
 
     // Re-check account ban (same check as in realmd)
